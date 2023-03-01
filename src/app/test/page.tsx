f9c057792164afd50615716a8cb6c18accd35216
Hamza <untilhamza@gmail.com>
2023-03-02 01:33:17 +0900

    do tailwind

---
+++ src/app/test/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Content from "../components/Content";
+
+const page = () => {
+  return (
+    <div>
+      <Content />
+    </div>
+  );
+};
+
+export default page;
