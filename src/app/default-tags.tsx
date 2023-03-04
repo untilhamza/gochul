d4c27c922a56b7c014e11fb46b5268e557962231
Hamza <untilhamza@gmail.com>
2023-03-04 20:02:11 +0900

    add default tags

---
+++ src/app/default-tags.tsx
@@ -0,0 +1,11 @@
+export default function DefaultTags() {
+  return (
+    <>
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0 maximum-scale=1.0, user-scalable=no"
+      />
+      <link href="/favicon.ico" rel="shortcut icon" />
+    </>
+  );
+}
