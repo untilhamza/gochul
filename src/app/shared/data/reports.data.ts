bc565cebc0d7df2b75b6491d5ddb1f6997861ab4
Hamza <untilhamza@gmail.com>
2023-03-11 13:28:16 +0900

    hard code sample reports data

---
+++ src/app/shared/data/reports.data.ts
@@ -0,0 +1,240 @@
+export const REPORTS = [
+  {
+    id: "1",
+    submissionDate: new Date(),
+    leader: { id: "1", name: "Angel" },
+    membersPresent: [
+      { id: "1", name: "Angel" },
+      { id: "2", name: "Bryan" },
+      { id: "3", name: "Cindy" },
+    ],
+    eventsActivities: "We had a great time!",
+    online: true,
+    prayerRequests: "Pray for the world",
+  },
+  {
+    id: "2",
+    submissionDate: new Date(),
+    leader: {
+      id: "4",
+      name: "David",
+    },
+    membersPresent: [
+      {
+        id: "4",
+        name: "David",
+      },
+      {
+        id: "5",
+        name: "Emily",
+      },
+      {
+        id: "6",
+        name: "Frank",
+      },
+    ],
+    eventsActivities: "We had a lot of fun!",
+    online: false,
+    prayerRequests: "Pray for our community",
+  },
+  {
+    id: "3",
+    submissionDate: new Date(),
+    leader: {
+      id: "7",
+      name: "Grace",
+    },
+    membersPresent: [
+      {
+        id: "7",
+        name: "Grace",
+      },
+      {
+        id: "8",
+        name: "Henry",
+      },
+      {
+        id: "9",
+        name: "Isabella",
+      },
+    ],
+    eventsActivities: "We learned a lot!",
+    online: true,
+    prayerRequests: "Pray for our families",
+  },
+  {
+    id: "4",
+    submissionDate: new Date(),
+    leader: {
+      id: "10",
+      name: "John",
+    },
+    membersPresent: [
+      {
+        id: "10",
+        name: "John",
+      },
+      {
+        id: "11",
+        name: "Kate",
+      },
+      {
+        id: "12",
+        name: "Leo",
+      },
+    ],
+    eventsActivities: "We did community service!",
+    online: false,
+    prayerRequests: "Pray for those in need",
+  },
+  {
+    id: "5",
+    submissionDate: new Date(),
+    leader: {
+      id: "13",
+      name: "Mary",
+    },
+    membersPresent: [
+      {
+        id: "13",
+        name: "Mary",
+      },
+      {
+        id: "14",
+        name: "Nathan",
+      },
+      {
+        id: "15",
+        name: "Olivia",
+      },
+    ],
+    eventsActivities: "We had a party!",
+    online: true,
+    prayerRequests: "Pray for our church",
+  },
+  {
+    id: "6",
+    submissionDate: new Date(),
+    leader: {
+      id: "16",
+      name: "Peter",
+    },
+    membersPresent: [
+      {
+        id: "16",
+        name: "Peter",
+      },
+      {
+        id: "17",
+        name: "Quinn",
+      },
+      {
+        id: "18",
+        name: "Rachel",
+      },
+    ],
+    eventsActivities: "We went on a retreat!",
+    online: false,
+    prayerRequests: "Pray for world peace",
+  },
+  {
+    id: "7",
+    submissionDate: new Date(),
+    leader: {
+      id: "19",
+      name: "Sam",
+    },
+    membersPresent: [
+      {
+        id: "19",
+        name: "Sam",
+      },
+      {
+        id: "20",
+        name: "Tina",
+      },
+      {
+        id: "21",
+        name: "Uma",
+      },
+    ],
+    eventsActivities: "We had a potluck!",
+    online: true,
+    prayerRequests: "Pray for our country",
+  },
+  {
+    id: "8",
+    submissionDate: new Date(),
+    leader: {
+      id: "22",
+      name: "Victor",
+    },
+    membersPresent: [
+      {
+        id: "22",
+        name: "Victor",
+      },
+      {
+        id: "23",
+        name: "Wendy",
+      },
+      {
+        id: "24",
+        name: "Xander",
+      },
+    ],
+    eventsActivities: "We went hiking!",
+    online: false,
+    prayerRequests: "Pray for those affected by natural disasters",
+  },
+  {
+    id: "9",
+    submissionDate: new Date(),
+    leader: {
+      id: "25",
+      name: "Yara",
+    },
+    membersPresent: [
+      {
+        id: "25",
+        name: "Yara",
+      },
+      {
+        id: "26",
+        name: "Zack",
+      },
+      {
+        id: "27",
+        name: "Alice",
+      },
+    ],
+    eventsActivities: "We watched a movie!",
+    online: true,
+    prayerRequests: "Pray for those struggling with mental health",
+  },
+  {
+    id: "10",
+    submissionDate: new Date(),
+    leader: {
+      id: "28",
+      name: "Ben",
+    },
+    membersPresent: [
+      {
+        id: "28",
+        name: "Ben",
+      },
+      {
+        id: "29",
+        name: "Carla",
+      },
+      {
+        id: "30",
+        name: "David",
+      },
+    ],
+    eventsActivities: "We had a game night!",
+    online: false,
+    prayerRequests: "Pray for the homeless",
+  },
+];
