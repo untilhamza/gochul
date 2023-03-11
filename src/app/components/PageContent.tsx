0b28fde70abe37aef91f548baeb31f02aa587614
Hamza <untilhamza@gmail.com>
2023-03-11 13:23:07 +0900

    render tabs as part of page content

--- src/app/components/PageContent.tsx
+++ src/app/components/PageContent.tsx
@@ -1,16 +1,19 @@
 import * as React from "react";
 import PageHeader from "@/app/components/PageHeader";
+import { PageTab } from "@/app/shared/interfaces/page.interfaces";
 
 export default function PageContent({
   children,
   pageName,
+  tabs = [],
 }: {
   children: React.ReactNode;
   pageName: string;
+  tabs: PageTab[];
 }) {
   return (
     <>
-      <PageHeader pageName={pageName} />
+      <PageHeader pageName={pageName} tabs={tabs} />
       <main className="flex-1 py-6 px-4 bg-sky-50">
         <div className="shadow-md rounded-md w-full md:max-w-[936px] mx-auto overflow-hidden p-3 bg-white border">
           {children}
