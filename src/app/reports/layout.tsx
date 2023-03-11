ae251ad9ad265e708cce34394fadd645d1fb957b
Hamza <untilhamza@gmail.com>
2023-03-11 13:24:22 +0900

    add tabs to reports layout

--- src/app/reports/layout.tsx
+++ src/app/reports/layout.tsx
@@ -1,6 +1,22 @@
 import * as React from "react";
 import PageContent from "@/app/components/PageContent";
+import { PageTab } from "@/app/shared/interfaces/page.interfaces";
+
+const reportTabs: PageTab[] = [
+  {
+    label: "All Reports",
+    path: "/reports",
+  },
+  {
+    label: "New Report",
+    path: "/reports/new",
+  },
+];
 
 export default function Content({ children }: { children: React.ReactNode }) {
-  return <PageContent pageName="Reports">{children}</PageContent>;
+  return (
+    <PageContent pageName="Reports" tabs={reportTabs}>
+      {children}
+    </PageContent>
+  );
 }
