210557e2aa644cc32bd24a3b9f85efff5c3f95f8
Hamza <untilhamza@gmail.com>
2023-03-11 13:26:14 +0900

    use report list item in reports table

--- src/app/reports/ReportsList.tsx
+++ src/app/reports/ReportsList.tsx
@@ -1,17 +1,13 @@
 import React from "react";
 import { Report } from "../shared/interfaces/reports.interfaces";
+import ReportItem from "./ReportItem";
 
 const ReportsList: React.FC<{ reports: Report[] }> = ({ reports }) => {
   return (
     <div className="flex flex-col gap-4 p-2">
       {reports.map((report, index) => {
         return (
-          <div
-            key={index}
-            className="w-full bg-blue-200 p-3 rounded-md hover:bg-blue-200/75 text-gray-800"
-          >
-            {report.submissionDate.toDateString()}
-          </div>
+          <ReportItem key={index} submissionDate={report.submissionDate} />
         );
       })}
     </div>
