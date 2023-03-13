86ebd518f0d7f9307f4c62c687a6a3d51b536b36
Hamza <untilhamza@gmail.com>
2023-03-13 11:48:43 +0900

    rename report list time file

---
+++ src/app/reports/ReportListItem.tsx
@@ -0,0 +1,19 @@
+"use client";
+import React from "react";
+import { useRouter } from "next/navigation";
+const ReportListItem: React.FC<{ submissionDate: Date; reportId: string }> = ({
+  submissionDate,
+  reportId,
+}) => {
+  const router = useRouter();
+  return (
+    <div
+      className="w-full bg-blue-200 p-3 rounded-md hover:bg-blue-200/75 text-gray-800"
+      onClick={() => router.push(`reports/${reportId}`)}
+    >
+      {submissionDate.toDateString()}
+    </div>
+  );
+};
+
+export default ReportListItem;
