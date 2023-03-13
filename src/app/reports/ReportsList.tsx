fa1e69c7b2e04bc994c7781cea9a2a21f4b459a2
Hamza <untilhamza@gmail.com>
2023-03-13 11:49:49 +0900

    use  data-super json to pass content to report list item

--- src/app/reports/ReportsList.tsx
+++ src/app/reports/ReportsList.tsx
@@ -1,13 +1,20 @@
 import React from "react";
+
 import { Report } from "../shared/interfaces/reports.interfaces";
-import ReportItem from "./ReportItem";
+import ReportListItem from "./ReportListItem";
 
 const ReportsList: React.FC<{ reports: Report[] }> = ({ reports }) => {
+  // Use "data-superjson" attribute to pass non-serializable props to client components
   return (
     <div className="flex flex-col gap-4 p-2">
       {reports.map((report, index) => {
         return (
-          <ReportItem key={index} submissionDate={report.submissionDate} />
+          <ReportListItem
+            key={index}
+            submissionDate={report.submissionDate}
+            reportId={report.id}
+            data-superjson
+          />
         );
       })}
     </div>
