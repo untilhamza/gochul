59fea7e8a8bebe011cccee576ed77371614aeee8
Hamza <untilhamza@gmail.com>
2023-03-13 11:48:19 +0900

    add super json plugin for prop serialization btn client and server

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@
 const nextConfig = {
   experimental: {
     appDir: true,
+    swcPlugins: [["next-superjson-plugin", {}]],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
