37db75406794dbcb7fdc869c5af05e8ace936fe7
Hamza <untilhamza@gmail.com>
2023-03-26 22:24:58 +0900

    add prisma schema

---
+++ src/lib/prisma/report.ts
@@ -0,0 +1,32 @@
+// import prisma from ".";
+
+// export async function getReports() {
+//   try {
+//     const reports = await prisma.report.findMany();
+//     return { reports };
+//   } catch (error) {
+//     return { error };
+//   }
+// }
+
+// export async function createReport(report: any) {
+//   try {
+//     const reportFromDB = await prisma.report.create({ data: report });
+//     return { report: reportFromDB };
+//   } catch (error) {
+//     return { error };
+//   }
+// }
+
+// export async function getReportById(id: any) {
+//   return await prisma.report.findUnique({ where: { id } });
+//   try {
+//     const report = await prisma.report.findUnique({
+//       where: { id },
+//       include: { tweets: true },
+//     });
+//     return { report };
+//   } catch (error) {
+//     return { error };
+//   }
+// }
