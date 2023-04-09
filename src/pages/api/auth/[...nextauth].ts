e6af80afd8fe39bb6f6adf9d1e9b27eeb6a4d9b1
Hamza <untilhamza@gmail.com>
2023-04-09 22:12:48 +0900

    add checks both for google auth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -11,6 +11,7 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      checks: ["pkce", "state"], //"both"
     }),
     // ...add more providers here
   ],
