4b810da1746b6cb3efe3caac5d22aa8b4c54b492
Hamza <untilhamza@gmail.com>
2023-05-26 18:47:37 +0900

    add group info layout

---
+++ src/app/leader/(group)/layout.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import PageContent from "@/components/PageContent";
+
+const groupTabs: PageTab[] = [
+  {
+    label: "Group Info",
+    path: "leader/group",
+  },
+  {
+    label: "Edit Group",
+    path: "leader/new-group",
+  },
+];
+
+export default function Content({ children }: { children: React.ReactNode }) {
+  return (
+    <PageContent pageName="Groups" tabs={groupTabs}>
+      {children}
+    </PageContent>
+  );
+}
