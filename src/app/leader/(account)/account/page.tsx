6f72b6254c09585dde2bc5618ea58b220c0bcaf1
Hamza <untilhamza@gmail.com>
2023-05-26 18:47:28 +0900

    add leader form

---
+++ src/app/leader/(account)/account/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LeaderForm from "@/components/settings/LeaderForm";
+
+const Page = () => {
+  return (
+    <>
+      <LeaderForm />
+    </>
+  );
+};
+
+export default Page;
