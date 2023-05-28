6585cc2c72eb4e5295584b7e23accd05e3e030ae
Hamza <untilhamza@gmail.com>
2023-05-28 23:20:42 +0900

    make groupId route a folder

--- src/pages/api/leader/[leaderId]/group/[groupId]/index.ts
+++ src/pages/api/leader/[leaderId]/group/[groupId]/index.ts
@@ -1,7 +1,7 @@
 import prisma from "@/lib/prisma";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { getServerSession } from "next-auth/next";
-import { authOptions } from "../../../auth/[...nextauth]";
+import { authOptions } from "../../../../auth/[...nextauth]";
 
 //PUT /api/group/id
 // Required fields in body:  active
