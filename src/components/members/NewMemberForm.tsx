e509a738029bb0e6ea19f7d9720fab9190fecf6d
Hamza <untilhamza@gmail.com>
2023-05-31 00:30:18 +0900

    implement a new member form component

--- src/components/members/NewMemberForm.tsx
+++ src/components/members/NewMemberForm.tsx
@@ -1,43 +1,122 @@
 "use client";
-import React, { useMemo } from "react";
+import React, { useMemo, useState } from "react";
 import countryList from "react-select-country-list";
 import Select from "react-select";
 import * as yup from "yup";
 import { useFormik } from "formik";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
+import { Member } from "@prisma/client";
+import axios from "axios";
+import Swal from "sweetalert2";
+import { useSession } from "next-auth/react";
+import Link from "next/link";
+import ErrorPageDetails from "../error/ErrorPageDetails";
+
+interface INewMemberFormProps {
+  groupId: string;
+}
 
 const FormSchema = yup.object().shape({
   firstName: yup.string().required("* First name is required"),
-  // lastName: yup.string().required("Last name is required"),
   country: yup.string().required("* Country is required"),
-  // occupation: yup.string().required("* Occupation is required"),
 });
 
-const NewMemberForm = () => {
+const NewMemberForm = ({ groupId }: INewMemberFormProps) => {
   const router = useRouter();
+  const searchParams = useSearchParams();
   const countryOptions = useMemo(() => countryList().getData(), []);
-  const occupationOptions = useMemo(
-    () => [
-      { value: "student", label: "Student" },
-      { value: "working", label: "Working Professional" },
-      { value: "family", label: "Family" },
-    ],
-    []
-  );
+  const { data: session, status } = useSession({
+    required: true,
+  });
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  console.log("groupId", groupId);
+
+  // read ?edit=MemberId from the url
+  const editMemberId = searchParams?.get("edit");
+  const editFirstName = searchParams?.get("firstName");
+  const editLastName = searchParams?.get("lastName");
+  const editCountry = searchParams?.get("country");
+
+  //@ts-ignore
+  const leaderId = session?.user?.id;
+
+  const handleMemberSubmit = async (values: Partial<Member>) => {
+    if (editMemberId) {
+      return handleMemberEdit(values);
+    }
+
+    const newMemberData = {
+      firstName: values.firstName,
+      lastName: values.lastName,
+      country: values.country,
+    };
+    //@ts-ignore
+    const userId = session?.user?.id;
+    try {
+      setIsLoading(true);
+      const result = await axios.post(
+        `/api/leader/${userId}/group/${groupId}/member`,
+        {
+          ...newMemberData,
+        }
+      );
+      formik.resetForm();
+      setIsLoading(false);
+      router.push(`/leader/members`);
+    } catch (err) {
+      console.log(err);
+      setIsLoading(false);
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Something went wrong!",
+      });
+    }
+  };
+
+  const handleMemberEdit = async (values: Partial<Member>) => {
+    const newMemberData = {
+      firstName: values.firstName,
+      lastName: values.lastName,
+      country: values.country,
+    };
+    //@ts-ignore
+    const userId = session?.user?.id;
+    try {
+      setIsLoading(true);
+      const result = await axios.put(
+        `/api/leader/${userId}/group/${groupId}/member/${editMemberId}`,
+        {
+          ...newMemberData,
+        }
+      );
+      formik.resetForm();
+      setIsLoading(false);
+      router.push(`/leader/members`);
+    } catch (err) {
+      console.log(err);
+      setIsLoading(false);
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Something went wrong!",
+      });
+    }
+  };
+
   const formik = useFormik({
     initialValues: {
-      firstName: "",
-      lastName: "",
-      country: "",
-      occupation: "",
+      firstName: editFirstName || "",
+      lastName: editLastName || "",
+      country: editCountry || "",
     },
     validationSchema: FormSchema,
-    onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2));
-      //reset form
-      formik.resetForm();
+    onSubmit: async (values) => {
+      await handleMemberSubmit(values);
     },
   });
+
   return (
     <div className="mt-10 sm:mt-0 ">
       <div className="md:grid md:grid-cols-3 md:gap-6">
@@ -70,7 +149,7 @@ const NewMemberForm = () => {
                       id="firstName"
                       onChange={formik.handleChange}
                       value={formik.values.firstName}
-                      autoComplete="given-name"
+                      autoComplete="off"
                       className="px-2 md:mt-1 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     />
                     {formik.touched.firstName && formik.errors.firstName && (
@@ -93,7 +172,7 @@ const NewMemberForm = () => {
                       id="lastName"
                       onChange={formik.handleChange}
                       value={formik.values.lastName}
-                      autoComplete="family-name"
+                      autoComplete="off"
                       className="px-2 mt-1 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     />
                     {formik.touched.lastName && formik.errors.lastName && (
@@ -111,13 +190,15 @@ const NewMemberForm = () => {
                       Country
                     </label>
                     <Select
-                      id="country"
-                      name="country"
+                      id={"country"}
+                      name={"country"}
                       options={countryOptions}
                       instanceId="react-select-3-live-region"
-                      value={countryOptions.find(
-                        (country) => country.value === formik.values.country
-                      )}
+                      value={
+                        countryOptions.find(
+                          (country) => country.value === formik.values.country
+                        ) || null
+                      }
                       onChange={(value) => {
                         formik.setFieldValue("country", value?.value || "");
                       }}
@@ -128,48 +209,36 @@ const NewMemberForm = () => {
                       </p>
                     )}
                   </div>
-
-                  <div className="col-span-6 sm:col-span-3">
-                    <label
-                      htmlFor="occupation"
-                      className="block text-sm font-medium leading-6 text-gray-900"
-                    >
-                      Occupation
-                    </label>
-                    <Select
-                      id="occupation"
-                      name="occupation"
-                      options={occupationOptions}
-                      instanceId="react-select-3-live-region"
-                      value={occupationOptions.find(
-                        (item) => item.value === formik.values.occupation
-                      )}
-                      onChange={(value) => {
-                        formik.setFieldValue("occupation", value?.value || "");
-                      }}
-                    />
-                    {formik.touched.occupation && formik.errors.occupation && (
-                      <p className="text-red-500 text-sm leading-6 font-medium">
-                        {formik.errors.occupation}
-                      </p>
-                    )}
-                  </div>
                 </div>
               </div>
               <div className="bg-gray-50 px-4 py-3 text-right sm:px-6 flex justify-end gap-2 mt-5">
-                <button
-                  type="submit"
-                  className="inline-flex justify-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
-                >
-                  Save
-                </button>
-                <button
-                  type="button"
-                  className="inline-flex justify-center rounded-md bg-red-600 py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-500"
-                  onClick={() => router.push("leader/members")}
-                >
-                  Cancel
-                </button>
+                {isLoading && (
+                  <button
+                    type="button"
+                    className="inline-flex justify-center rounded-md bg-blue-500 py-2 px-3 font-semibold text-white w-full md:w-auto"
+                    disabled={isLoading}
+                  >
+                    Loading ...
+                  </button>
+                )}
+                {!isLoading && (
+                  <>
+                    {" "}
+                    <Link
+                      type="button"
+                      className="inline-flex justify-center rounded-md bg-red-500 py-2 px-3 font-semibold text-white hover:bg-red-600 w-full md:w-auto"
+                      href="leader/members"
+                    >
+                      Cancel
+                    </Link>
+                    <button
+                      type="submit"
+                      className="bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-3 rounded-md inline-flex justify-center w-full md:w-auto"
+                    >
+                      Save
+                    </button>
+                  </>
+                )}
               </div>
             </div>
           </form>
