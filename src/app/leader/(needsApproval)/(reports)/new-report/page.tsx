3a9139487fd9fec43496a6d68f367a62ed886ee1
Hamza <untilhamza@gmail.com>
2023-06-01 22:13:05 +0900

    create newPage server side component

--- src/app/leader/(needsApproval)/(reports)/new-report/page.tsx
+++ src/app/leader/(needsApproval)/(reports)/new-report/page.tsx
@@ -1,9 +1,37 @@
+import prisma from "@/lib/prisma";
+import { authOptions, IUserSession } from "@/pages/api/auth/[...nextauth]";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+
 import ReportForm from "./NewReport";
 
-const NewReportPage: React.FC = () => {
+const NewReportPage = async () => {
+  const session: IUserSession = await getServerSession(authOptions);
+  const user = session?.user;
+
+  if (!user) return redirect("/");
+
+  const leaderId = user?.id;
+
+  const group = await prisma.group.findFirst({
+    where: {
+      leaderId: leaderId,
+      active: true,
+    },
+  });
+
+  const groupId = group?.id;
+
+  //NOTE: this page should not be accessible if there is no group
+  if (!groupId) throw new Error("No group found");
+
+  const members = await prisma.member.findMany({
+    where: { groupId: groupId as string, isDeleted: false },
+  });
+
   return (
     <>
-      <ReportForm />
+      <ReportForm leaderId={leaderId} groupId={groupId} members={members} />
     </>
   );
 };
