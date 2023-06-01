495904d3b0424e68857c67e5cc62907d50252922
Hamza <untilhamza@gmail.com>
2023-06-01 22:13:55 +0900

    add api utils

---
+++ src/utils/apiUtils.ts
@@ -0,0 +1,29 @@
+import prisma from "@/lib/prisma";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+
+export const isAdminOrLeader = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
+  const session = await getServerSession(req, res, authOptions);
+  if (!session) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+
+  const user = session.user;
+
+  const userFromDB = await prisma.gochulUser.findUnique({
+    where: { email: user?.email || "" },
+  });
+
+  if (
+    !userFromDB ||
+    (userFromDB.role !== "ADMIN" && userFromDB.role !== "LEADER")
+  ) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+
+  return userFromDB;
+};
