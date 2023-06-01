e55b8b56647fd28056a4e914167f4c61a93ca340
Hamza <untilhamza@gmail.com>
2023-06-01 22:08:29 +0900

    correct usage of getserverside session and direct

--- src/app/leader/(needsApproval)/(members)/members/page.tsx
+++ src/app/leader/(needsApproval)/(members)/members/page.tsx
@@ -1,10 +1,16 @@
-import { IUserSession } from "@/pages/api/auth/[...nextauth]";
+import { authOptions, IUserSession } from "@/pages/api/auth/[...nextauth]";
 import { getServerSession } from "next-auth";
 import prisma from "@/lib/prisma";
 import MembersPageContent from "./MembersPageContent";
+import { redirect } from "next/navigation";
 
 const GroupMembersListPage = async () => {
-  const session: IUserSession = await getServerSession();
+  const session: IUserSession = await getServerSession(authOptions);
+
+  if (!session) {
+    return redirect("/");
+  }
+
   const user = session?.user;
   const userId = user?.id;
 
